c977b16bdba1ddcd40f072936c17016376355ed6
gartlen <valentina.vargas.carvajal10@gmail.com>
2023-09-05 22:26:59 -0500

    Clase de 11 a 14, corregir error

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,12 +3,26 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
+    // SHopping Cart . Increment quantity
     const [count, setCount] = useState(0)
-
+    
+    // Product Detail . Show Product
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+    
+    // Product Detail . Show Product
+    const [productToShow, setProductToShow] = useState({})
+    
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
